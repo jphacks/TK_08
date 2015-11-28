ce7813bdedf2dcf03ccd4152412a390364b9ad63
go <go05sato@gmail.com>
2015-11-29 01:34:25 +0900

    iBeacon受信、majorIDリスト作成実装

--- iOS/AirMeet/AirMeet/MainViewController.swift
+++ iOS/AirMeet/AirMeet/MainViewController.swift
@@ -7,11 +7,23 @@
 //
 
 import UIKit
+//------go
+import CoreLocation
+//
 
-class MainViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
+class MainViewController: UIViewController,UITableViewDelegate, UITableViewDataSource ,CLLocationManagerDelegate{
     
     let appDelegate:AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
     
+    //--------go
+    //UUIDは送信機側とフォーマットも合わせる
+    let proximityUUID = NSUUID(UUIDString:"B9407F30-F5F8-466E-AFF9-33333B57FE6D")
+    var region  = CLBeaconRegion()
+    var manager = CLLocationManager()
+    
+    //majorIDリスト用のグローバル変数
+    var majorIDList:[NSNumber] = []
+    //-----------
     
     @IBOutlet weak var backImageView: UIImageView!
     @IBOutlet weak var imageImageView: UIImageView!
@@ -48,15 +60,113 @@ class MainViewController: UIViewController,UITableViewDelegate, UITableViewDataS
         EventTableView.delegate = self
         EventTableView.dataSource = self
         
- 
+        //------go
+        //majorIIをプリントできるように
+        
+        //CLBeaconRegionを生成
+        region = CLBeaconRegion(proximityUUID:proximityUUID!,identifier:"AirMeet")
+        //デリゲートの設定
+        manager.delegate = self
+        
+        switch CLLocationManager.authorizationStatus() {
+        case .Authorized, .AuthorizedWhenInUse:
+            //iBeaconによる領域観測を開始する
+            print("観測開始")
+            //self.status.text = "Starting Monitor"
+            self.manager.startRangingBeaconsInRegion(self.region)
+        case .NotDetermined:
+            print("許可承認")
+            //self.status.text = "Starting Monitor"
+            //デバイスに許可を促す
+            
+            let deviceVer = UIDevice.currentDevice().systemVersion
+            if(Int(deviceVer.substringToIndex(deviceVer.startIndex.advancedBy(1))) >= 8){
+                
+                //iOS8以降は許可をリクエストする関数をCallする
+                self.manager.requestAlwaysAuthorization()
+                print("OK")
+            }else{
+                self.manager.startRangingBeaconsInRegion(self.region)
+            }
+        case .Restricted, .Denied:
+            //デバイスから拒否状態
+            print("Restricted")
+            //self.status.text = "Restricted Monitor"
+        }
+        
+        print(majorIDList)
+        //---------------
+        
         
         //会場追加
         let event:EventModel = EventModel(eventName: "JPHacks-東京会場", roomName: "東京大学 本郷キャンパス215教室", childNumber: 50, eventDescription: "aaa")
         events.append(event)
         
         
- 
+        
     }
+    
+    ///-----go
+    func locationManager(manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], inRegion region:CLBeaconRegion) {
+        
+        majorIDList = []
+        
+        //print(beacons)
+        
+        if(beacons.count == 0) {
+            print("nothing")
+            return
+        }
+        //複数あった場合は一番先頭のものを処理する
+        let beacon = beacons[0]
+        
+        for i in 0..<beacons.count{
+            majorIDList.append(beacons[i].major)
+        }
+        
+        if(beacons.count != 0){
+            let set = NSOrderedSet(array: majorIDList)
+            majorIDList = set.array as! [NSNumber]
+            print(majorIDList)
+        }
+        
+        /*
+        beaconから取得できるデータ
+        proximityUUID   :   regionの識別子
+        major           :   識別子１
+        minor           :   識別子２
+        proximity       :   相対距離
+        accuracy        :   精度
+        rssi            :   電波強度
+        */
+        if (beacon.proximity == CLProximity.Unknown) {
+            //self.distance.text = "Unknown Proximity"
+            reset()
+            return
+        } else if (beacon.proximity == CLProximity.Immediate) {
+            //self.distance.text = "Immediate"
+        } else if (beacon.proximity == CLProximity.Near) {
+            //self.distance.text = "Near"
+        } else if (beacon.proximity == CLProximity.Far) {
+            //self.distance.text = "Far"
+        }
+        //self.status.text   = "OK"
+        //self.uuid.text     = beacon.proximityUUID.UUIDString
+        //self.major.text    = "\(beacon.major)"
+        //self.minor.text    = "\(beacon.minor)"
+        //self.accuracy.text = "\(beacon.accuracy)"
+        //self.rssi.text     = "\(beacon.rssi)"
+    }
+    
+    func reset(){
+        //self.status.text   = "none"
+        //self.uuid.text     = "none"
+        //self.major.text    = "none"
+        //self.minor.text    = "none"
+        //self.accuracy.text = "none"
+        //self.rssi.text     = "none"
+    }
+    ///-----
     
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath:NSIndexPath) -> UITableViewCell {
@@ -80,7 +190,7 @@ class MainViewController: UIViewController,UITableViewDelegate, UITableViewDataS
     
     //cellが選択されたとき
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-       // selectGroup = appDelegate.groupArray[indexPath.row] as! String
+        // selectGroup = appDelegate.groupArray[indexPath.row] as! String
         //画面遷移
         //performSegueWithIdentifier("showGroup",sender: nil)
         
@@ -105,7 +215,7 @@ class MainViewController: UIViewController,UITableViewDelegate, UITableViewDataS
         
         self.navigationController?.pushViewController(parentViewController, animated: true)
     }
-
+    
     //子
     @IBAction func ChildButton(sender: AnyObject) {
         
@@ -115,7 +225,7 @@ class MainViewController: UIViewController,UITableViewDelegate, UITableViewDataS
         self.navigationController?.pushViewController(childViewController, animated: true)
         
     }
-
+    
     //Meet
     @IBAction func MeetButton(sender: AnyObject) {
         
@@ -138,7 +248,7 @@ class MainViewController: UIViewController,UITableViewDelegate, UITableViewDataS
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
