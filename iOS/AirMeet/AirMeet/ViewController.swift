2f63c5b0bfca223ed0299d1e26f3a1fbcd141939
koooootake <koooootake@koooootake-MacBookPro.local>
2015-11-28 09:47:51 +0900

    Add iOS Project

---
+++ iOS/AirMeet/AirMeet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AirMeet
+//
+//  Created by koooootake on 2015/11/28.
+//  Copyright © 2015年 koooootake. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
