a8cd47ce042de2190558bb60abf96dbda77f0e31
川端彬子 <20.10.ak.tkb@gmail.com>
2015-12-08 12:38:22 +0900

    :art: Fix the color of selected Tab bar icon and text

--- iOS/AirMeet/AirMeet/OriginalTabBarController.swift
+++ iOS/AirMeet/AirMeet/OriginalTabBarController.swift
@@ -14,18 +14,22 @@ class OriginalTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         ///（kmdr,momoka）
+        
+        // 選択時のタブバーアイコン・テキストの色
+        let selectedColor:UIColor = UIColor(red: 65.0/255.0, green: 168.0/255.0, blue: 186.0/255.0, alpha: 1)//水色
+
         ///ボタンをいいかんじに
         let fontFamily: UIFont! = UIFont(name: "Hiragino Kaku Gothic ProN",size:10)
         
         // 文字色とフォント変えたい
-        let selectedAttributes = [NSFontAttributeName: fontFamily, NSForegroundColorAttributeName: UIColor.whiteColor()]
+        let selectedAttributes = [NSFontAttributeName: fontFamily, NSForegroundColorAttributeName: selectedColor]
         //let nomalAttributes = [NSFontAttributeName: fontFamily, NSForegroundColorAttributeName: UIColor.whiteColor()]
         
         UITabBarItem.appearance().setTitleTextAttributes(selectedAttributes, forState: UIControlState.Selected)
         //UITabBarItem.appearance().setTitleTextAttributes(nomalAttributes, forState: UIControlState.Normal)
         
         // アイコンの色
-        UITabBar.appearance().tintColor = UIColor.whiteColor()
+        UITabBar.appearance().tintColor = selectedColor
         
         // 背景色
         UITabBar.appearance().barTintColor = UIColor(red: 128/255.0, green: 204/255.0, blue: 223/255.0, alpha: 1.0)
